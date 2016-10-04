771cddb693a0df3bb76311a8e659ac5e2cc21504
Zowie Haugaard <zdh27@nau.edu>
2016-10-04 10:06:19 -0700

    Camera can now be anywhere in the scene file

--- rayCaster.c
+++ rayCaster.c
@@ -13,7 +13,7 @@ Ray Caster
  #include "parser.h"
 
 
- static inline double sqr(double v) {
+static inline double sqr(double v) {
    return v*v;
  }
 
@@ -21,7 +21,7 @@ static inline double dot(double* v1, double* v2){
   return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
 
- double* sub(double* v1, double* v2){
+static inline double* sub(double* v1, double* v2){
   static double result[3];
   result[0] = v1[0] - v2[0];
   result[1] = v1[1] - v2[1];
@@ -53,52 +53,7 @@ static inline double dot(double* v1, double* v2){
 
  double sphere_intersection(double* Ro, double* Rd,
  			     double* C, double r) {
-             // Step 1. Find the equation for the object you are
-             // interested in..  (e.g., sphere)
-             //
-             // x^2 + y^2 + z^2 = r^2
-             //
-             // Step 2. Parameterize the equation with a center point
-             // if needed
-             //
-             // (x-Cx)^2 + (y-Cy) + (z-Cz)^2 = r^2
-             //
-             // Step 3. Substitute the eq for a ray into our object
-             // equation.
-             //
-             // (Rox + t*Rdx - Cx)^2 + (Roy + t*Rdy - Cy)^2) (Roz + t*Rdz - Cz)^2 - r^2 = 0
-             //
-             // Step 4. Solve for t.
-             //
-             // Step 4a. Rewrite the equation (flatten).
-             //
-             // -r^2 +
-             // t^2 * Rdx^2 +
-             // t^2 * Rdy^2 +
-             // t^2 * Rdz^2 +
-             // 2*t * Rox * Rdx -
-             // 2*t * Rdx * Cx +
-             // 2*t * Roy * Rdy -
-             // 2*t * Rdy * Cy +
-             // 2*t * Roz * Rdz -
-             // 2*t * Rdz * Cz +
-             // Rox^2 -
-             // 2*Rox*Cx +
-             // Cx^2 +
-             // Roy^2 -
-             // 2*Roy*Cy +
-             // Cy^2 +
-             // Roz^2 -
-             // 2*Roz*Cz +
-             // Cz^2 = 0
-             //
-             // Steb 4b. Rewrite the equation in terms of t.
-             //
-             // t^2 * (Rdx^2 + rdy^2 + Rdz^2) +
-             // t * (2 * (Rox * Rdx - Rdx * Cx + Roy * Rdy - Rdy * Cy + Roz * Rdz - Rdz * Cz)) +
-             // Rox^2 - 2*Rox*Cx + Cx^2 + Roy^2 - 2*Roy*Cy + Cy^2 + Roz^2 - 2*Roz*Cz + Cz^2 - r^2 = 0
-             //
-             // Use the quadratic equation to solve for t..
+
              double a = (sqr(Rd[0]) + sqr(Rd[1]) + sqr(Rd[2]));
              double b = (2 * (Ro[0] * Rd[0] - Rd[0] * C[0] + Ro[1] * Rd[1] - Rd[1] * C[1] + Ro[2] * Rd[2] - Rd[2] * C[2]));
              double c = sqr(Ro[0]) - 2*Ro[0]*C[0] + sqr(C[0]) + Ro[1] - 2*Ro[1]*C[1] + sqr(C[1]) + sqr(Ro[2]) - 2*Ro[2]*C[2] + sqr(C[2]) - sqr(r);
@@ -119,67 +74,6 @@ static inline double dot(double* v1, double* v2){
 }
 
 
- double cylinder_intersection(double* Ro, double* Rd,
- 			     double* C, double r) {
-   // Step 1. Find the equation for the object you are
-   // interested in..  (e.g., cylinder)
-   //
-   // x^2 + z^2 = r^2
-   //
-   // Step 2. Parameterize the equation with a center point
-   // if needed
-   //
-   // (x-Cx)^2 + (z-Cz)^2 = r^2
-   //
-   // Step 3. Substitute the eq for a ray into our object
-   // equation.
-   //
-   // (Rox + t*Rdx - Cx)^2 + (Roz + t*Rdz - Cz)^2 - r^2 = 0
-   //
-   // Step 4. Solve for t.
-   //
-   // Step 4a. Rewrite the equation (flatten).
-   //
-   // -r^2 +
-   // t^2 * Rdx^2 +
-   // t^2 * Rdz^2 +
-   // 2*t * Rox * Rdx -
-   // 2*t * Rdx * Cx +
-   // 2*t * Roz * Rdz -
-   // 2*t * Rdz * Cz +
-   // Rox^2 -
-   // 2*Rox*Cx +
-   // Cx^2 +
-   // Roz^2 -
-   // 2*Roz*Cz +
-   // Cz^2 = 0
-   //
-   // Steb 4b. Rewrite the equation in terms of t.
-   //
-   // t^2 * (Rdx^2 + Rdz^2) +
-   // t * (2 * (Rox * Rdx - Rdx * Cx + Roz * Rdz - Rdz * Cz)) +
-   // Rox^2 - 2*Rox*Cx + Cx^2 + Roz^2 - 2*Roz*Cz + Cz^2 - r^2 = 0
-   //
-   // Use the quadratic equation to solve for t..
-   double a = (sqr(Rd[0]) + sqr(Rd[2]));
-   double b = (2 * (Ro[0] * Rd[0] - Rd[0] * C[0] + Ro[2] * Rd[2] - Rd[2] * C[2]));
-   double c = sqr(Ro[0]) - 2*Ro[0]*C[0] + sqr(C[0]) + sqr(Ro[2]) - 2*Ro[2]*C[2] + sqr(C[2]) - sqr(r);
-
-   double det = sqr(b) - 4 * a * c;
-   if (det < 0) return -1;
-
-   det = sqrt(det);
-
-   double t0 = (-b - det) / (2*a);
-   if (t0 > 0) return t0;
-
-   double t1 = (-b + det) / (2*a);
-   if (t1 > 0) return t1;
-
-   return -1;
- }
-
-
  //represents a single pixel object
  typedef struct RGB {
    unsigned char r, g, b;
@@ -197,15 +91,6 @@ static inline double dot(double* v1, double* v2){
    printf("%d\n", getSize());
    objects[getSize()] = NULL;
 
-   if(objects[0]->kind != 0){
-     printf("Error: please provide a camera object\n");
-     exit(1);
-   }
-   if(objects[0]->camera.width > 0 && objects[0]->camera.height > 0){
-
-   }
-
-
    //Make sure the right number of arguments was supplied
    if(argc < 5 || argc > 5) {
      perror("usage: ./cast width height object-file.json output-file.ppm \n");
@@ -216,8 +101,13 @@ static inline double dot(double* v1, double* v2){
 
    double *curcolor;
 
-   int width = 300;
-   int height = 300;
+
+   int width = atoi(argv[1]);
+   int height = atoi(argv[2]);
+
+   if(width == 0 || height == 0){
+     printf("Please provide valid width and height values\n");
+   }
    printf("%d\n", width);
 
    //create and allociate space for the ppmimage
@@ -230,23 +120,28 @@ static inline double dot(double* v1, double* v2){
 
    double cx = 0;
    double cy = 0;
-   double h;
-   double w;
+   float h = NAN;
+   float w = NAN;
 
-   if(objects[0]->kind != 0){
-     printf("Error: please provide a camera object\n");
-     exit(1);
+   for (int i=0; objects[i] != NULL; i += 1){
+      if(objects[i]-> kind == 0){
+        if(objects[i]->camera.width > 0 && objects[i]->camera.height > 0){
+          w = objects[i]->camera.width;
+          h = objects[i]->camera.height;
+        }
+        else {
+          printf("Error: please provide a valid camera size\n");
+          exit(1);
+        }
+      }
    }
-   if(objects[0]->camera.width > 0 && objects[0]->camera.height > 0){
-     w = objects[0]->camera.width;
-     h = objects[0]->camera.height;
+   if(isnan(h) || isnan(w)){
+     printf("Error: Please provide a valid camera object\n");
    }
-   else printf("Error: please provide a valid camera size\n");
 
-   int M = height;
-   int N = width;
+   printf("%f %f\n",w, h);
 
-   (void) fprintf(outFile, "P6\n%d %d\n255\n", N, M);
+   (void) fprintf(outFile, "P6\n%d %d\n255\n", width, height);
 
    double pixheight = h / height;
    double pixwidth = w / width;
@@ -290,15 +185,18 @@ static inline double dot(double* v1, double* v2){
        outcolor[0] = (char) (255 * curcolor[0]);
        outcolor[1] = (char) (255 * curcolor[1]);
        outcolor[2] = (char) (255 * curcolor[2]);
+
+       int curPix = (height - y) * width + x;
+
        if (best_t > 0 && best_t != INFINITY) {
-         image.data[(height - y) * width + x].r = outcolor[0];
-         image.data[(height - y) * width + x].g = outcolor[1];
-         image.data[(height - y) * width + x].b = outcolor[2];
+         image.data[curPix].r = outcolor[0];
+         image.data[curPix].g = outcolor[1];
+         image.data[curPix].b = outcolor[2];
        }
        else {
-         image.data[(height - y) * width + x].r = backcolor[0];
-         image.data[(height - y) * width + x].g = backcolor[1];
-         image.data[(height - y) * width + x].b = backcolor[2];
+         image.data[curPix].r = backcolor[0];
+         image.data[curPix].g = backcolor[1];
+         image.data[curPix].b = backcolor[2];
        }
      }
 
